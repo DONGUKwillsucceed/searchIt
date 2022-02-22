03d01f60365aff541b0afcef72b31632ba514be2
Johncakes <johnnygamesover9000@gmail.com>
2022-02-22 17:17:06 +0900

    Map markers
    - Added user marker
    - Added printer marker without color distinguishing

--- src/map/PrinterMarker.tsx
+++ src/map/PrinterMarker.tsx
@@ -1,5 +1,6 @@
 import { IPrinterData } from "../Interfaces";
 import { MapMarker } from "react-kakao-maps-sdk";
+import mapPin from "../Images/map-pin.png";
 
 export default function PrinterMarker(props: {
   printerData: IPrinterData[] | never[];
@@ -7,6 +8,13 @@ export default function PrinterMarker(props: {
   const markerList = props.printerData.map((printer) => {
     return (
       <MapMarker
+        image={{
+          src: mapPin,
+          size: {
+            width: 32,
+            height: 36.5,
+          },
+        }}
         key={printer.id}
         position={{ lat: printer.lat, lng: printer.lon }}
       ></MapMarker>
--- src/map/UserMarker.tsx
+++ src/map/UserMarker.tsx
@@ -1,17 +1,18 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { IUserLoc } from "../Interfaces";
+import User from "../Images/User.svg";
 
 export default function UserMarker(props: { userLoc: IUserLoc }) {
   return (
     <MapMarker
-      position={props.userLoc.center}
       image={{
-        src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png",
-        size: { width: 64, height: 69 },
-        options: {
-          className: "bg-red-500",
+        src: User,
+        size: {
+          width: 16,
+          height: 16,
         },
       }}
+      position={props.userLoc.center}
     />
   );
 }
