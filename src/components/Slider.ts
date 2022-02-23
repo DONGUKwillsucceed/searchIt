f04fcd1b55eebd7d1c8dec99c282bcd658fc444a
Johncakes <johnnygamesover9000@gmail.com>
2022-02-23 20:57:16 +0900

    Create Slider.ts

---
+++ src/components/Slider.ts
@@ -0,0 +1,29 @@
+export default function Slider() {
+  const slider: Element | null = document.querySelector(".items");
+  let isDown: Boolean = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+  });
+}
