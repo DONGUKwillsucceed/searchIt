e0e56c5b4ac4d5a589821e51c5caf7e63b809fef
KYUUUW <duffufk1202@naver.com>
2022-04-29 13:39:27 +0900

    Fix array usage and exception control

--- src/pages/api/printer/universities.ts
+++ src/pages/api/printer/universities.ts
@@ -1,45 +1,44 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { LogicalError } from "../errors";
 
-export async function countPrinterUniversities(){
-    const prisma = new PrismaClient();
-    let data = [];
-    const result = await prisma.tagTypes.findFirst({
-        where : {
-            type : 'university'
+export async function countPrinterUniversities() {
+  const prisma = new PrismaClient();
+  const queryResult = await prisma.tagTypes.findFirst({
+    where: {
+      type: "university",
+    },
+    select: {
+      Tag: {
+        select: {
+          value: true,
+          _count: {
+            select: {
+              PrintZone_Tag: true,
+            },
+          },
         },
-        select : {
-            Tag:{
-                select:{
-                    value:true,
-                    _count:{
-                        select:{
-                            PrintZone_Tag:true
-                        }
-                    }
-                }
-            }
-        }
-    });
-    if(!result){
-        return ;
-    }
-    const unis = result.Tag
-    for(const uni of unis){
-        const {PrintZone_Tag} = uni._count;
-        const info = {
-            college : uni.value,
-            count : PrintZone_Tag
-        }
-        data.push(info);
-    }
-    return data;
+      },
+    },
+  });
+
+  if (!queryResult) {
+    throw new LogicalError("Tag type 에 university 가 존재하지 않음");
+  }
+
+  return queryResult.Tag.map((t) => ({
+    university: t.value,
+    count: t._count,
+  }));
 }
 
-
-export default async (req : NextApiRequest, res : NextApiResponse)=>{
-    if(req.method === 'GET'){
-        const data = await countPrinterUniversities();
-        res.status(200).json(data);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const data = await countPrinterUniversities();
+      res.status(200).json(data);
+    } catch (err: any) {
+      res.status(500).send(err.toJSON ? err.toJSON() : null);
     }
-}
+  }
+};
