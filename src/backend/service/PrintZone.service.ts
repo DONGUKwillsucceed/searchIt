37586a0f741b27b8cb64ff7821045855c6a8a675
@kyuuuw <duffufk1202@naver.com>
2022-05-01 17:31:37 +0900

    Add full text search

--- src/backend/service/PrintZone.service.ts
+++ src/backend/service/PrintZone.service.ts
@@ -38,5 +38,104 @@ class PrintZoneService {
 
     return queryResult.PrintZone_Tag.map((r) => r.PrintZones).map((pz) => pz);
   }
+
+  async findUnique(id: string) {
+    const queryResult = await db.printZones.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!queryResult) {
+      throw new NotFoundError("No PrintZone with id");
+    }
+    const { writer_ip, ...rest } = queryResult;
+    return rest;
+  }
+
+  async searchByTag(keyword: string) {
+    const tagQueryResult = await db.tag.findMany({
+      where: {
+        value: {
+          search: `${keyword}*`,
+        },
+      },
+    });
+    const pzTagQueryWithPrintZone = await Promise.all(
+      tagQueryResult.map((q) =>
+        db.printZone_Tag.findMany({
+          where: {
+            Tag_id: q.id,
+          },
+          include: {
+            PrintZones: true,
+          },
+        })
+      )
+    );
+    return pzTagQueryWithPrintZone.flatMap((pzts) =>
+      pzts.map((pzt) => pzt.PrintZones)
+    );
+  }
+
+  async searchByCompany(keyword: string) {
+    const queryResult = await db.printZones.findMany({
+      where: {
+        company: {
+          search: `${keyword}*`,
+        },
+      },
+    });
+    return queryResult;
+  }
+
+  async searchByAddress(keyword: string) {
+    const queries = [];
+    queries.push(
+      db.printZones.findMany({
+        include: {
+          AreaCode: true,
+        },
+        where: {
+          AreaCode: {
+            ko_area_2: {
+              search: `${keyword}*`,
+            },
+          },
+        },
+      })
+    );
+    queries.push(
+      db.printZones.findMany({
+        include: {
+          AreaCode: true,
+        },
+        where: {
+          AreaCode: {
+            ko_area_3: {
+              search: `${keyword}*`,
+            },
+          },
+        },
+      })
+    );
+    queries.push(
+      db.printZones.findMany({
+        where: {
+          address_detail: {
+            search: `${keyword}*`,
+          },
+        },
+      })
+    );
+    return (await Promise.all(queries)).flatMap((pz) => pz);
+  }
+
+  async search(keyword: string) {
+    const queries = [];
+    queries.push(this.searchByTag(keyword));
+    queries.push(this.searchByCompany(keyword));
+    queries.push(this.searchByAddress(keyword));
+    return (await Promise.all(queries)).flatMap((pz) => pz);
+  }
 }
 export const printZoneService = new PrintZoneService();
