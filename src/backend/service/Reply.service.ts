3ed69b5099b0d737d7314d33e93d8426c4f15b08
LEE KYU WON <duffufk1202@naver.com>
2022-05-13 17:45:10 +0900

    Fix route name

---
+++ src/backend/service/Reply.service.ts
@@ -0,0 +1,14 @@
+import { NotFoundError } from "../errors";
+import { db } from "../db";
+
+class ReplyService {
+  add() {
+    // TODO
+  }
+
+  findByPrintZoneId(pzId: string) {
+    // TODO
+  }
+}
+
+export const noticeService = new ReplyService();
